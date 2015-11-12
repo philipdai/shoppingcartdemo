84daec6f0f4eca735301fae1fcd87ffe96a0853f
Philip Dai <smartielion@gmail.com>
2015-11-12 01:27:09 -0600

    Implement cart and checkout functions

--- app/checkout/checkout.js
+++ app/checkout/checkout.js
@@ -2,12 +2,14 @@
  
 angular.module('checkout', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
+
 	$routeProvider.when('/checkout', {
 			templateUrl: '/checkout/checkout.html',
 			controller: 'CheckoutCtrl'
 	});
 }])
  
-.controller('CheckoutCtrl', ['$scope', function($scope) {
-    
+.controller('CheckoutCtrl', ['$scope','CommonProp',function($scope,CommonProp) {
+	$scope.items = CommonProp.getItems();   
+	$scope.total = CommonProp.getTotal();
 }]);
